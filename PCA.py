9095b9e32aced103a5f4505ece4567062c5405c6
anaraSeishin <anara.75197@gmail.com>
2017-06-12 21:30:07 +0300

    PCA

--- PCA.py
+++ PCA.py
@@ -1,35 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
 from sklearn.datasets import load_iris
+
 iris = load_iris()
 X = iris.data
 y = iris.target
 
-mean = [[], [], []]
-Xc0 = [[], [], []]
-
-m = (X[:, 0].mean(), X[:, 1].mean(), X[:, 2].mean(), X[:, 3].mean())
-Xcentered0 = X[:, 0] - m[0]
-Xcentered1 = X[:, 1] - m[1]
-Xcentered2 = X[:, 2] - m[2]
-Xcentered3 = X[:, 3] - m[3]
-
 Xcentered = (X - X.mean(axis=0)).T
-print(Xcentered)
-print("Mean vector: ", m)
-
-
 covmat = np.cov(Xcentered)
-print(covmat)
-
 
 # снижение размерности
 _, vecs = np.linalg.eig(covmat)
-print(vecs)
 v = vecs.T
 Xnew = np.dot(v, Xcentered)
-print("Xnew   ", Xnew)
+
 colors = ['navy', 'turquoise', 'darkorange']
 plt.figure(figsize=(8, 8))
 
